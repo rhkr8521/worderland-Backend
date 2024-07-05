9cfde702d4cb371aad46e139fe1a157073f9ae7a
rhkr8521 <rhkr8521@gmail.com>
2024-07-05 20:30:26 +0900

    Initial commit

---
+++ src/main/java/com/rhkr8521/iccas_question/IccasQuestionApplication.java
@@ -0,0 +1,13 @@
+package com.rhkr8521.iccas_question;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IccasQuestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IccasQuestionApplication.class, args);
+	}
+
+}
