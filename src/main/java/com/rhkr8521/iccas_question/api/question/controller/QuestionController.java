4a9cfccd77933f384464af591c5cafcac7665bc7
rhkr8521 <rhkr8521@gmail.com>
2024-07-07 22:18:39 +0900

    [FIX] Change Random Count

--- src/main/java/com/rhkr8521/iccas_question/api/question/controller/QuestionController.java
+++ src/main/java/com/rhkr8521/iccas_question/api/question/controller/QuestionController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,10 +20,10 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping("/api/question/{theme}")
-    public ApiResponse<QuestionDTO> getQuestionByThemeAndStage(@PathVariable String theme, @RequestParam Long stage) {
-        Optional<QuestionDTO> question = questionService.getRandomQuestionByThemeAndStage(theme, stage);
-        if (question.isPresent()) {
-            return ApiResponse.success(SuccessStatus.SEND_QUESTION_SUCCESS, question.get());
+    public ApiResponse<List<QuestionDTO>> getQuestionsByThemeAndStage(@PathVariable String theme, @RequestParam Long stage) {
+        List<QuestionDTO> questions = questionService.getRandomQuestionsByThemeAndStage(theme, stage);
+        if (!questions.isEmpty()) {
+            return ApiResponse.success(SuccessStatus.SEND_QUESTION_SUCCESS, questions);
         } else {
             return ApiResponse.fail(ErrorStatus.NOT_FOUND_QUESTION.getStatusCode(), ErrorStatus.NOT_FOUND_QUESTION.getMessage());
         }
