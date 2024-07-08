6d8466badc2e68aebbcb96907ca477bb665029c0
rhkr8521 <rhkr8521@gmail.com>
2024-07-08 21:36:21 +0900

    [FIX] Refactoring Error Exception

--- src/main/java/com/rhkr8521/iccas_question/common/response/ErrorStatus.java
+++ src/main/java/com/rhkr8521/iccas_question/common/response/ErrorStatus.java
@@ -24,7 +24,8 @@ public enum ErrorStatus {
      * 500 SERVER_ERROR
      */
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예상치 못한 서버 에러가 발생했습니다."),
-    BAD_GATEWAY_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "일시적인 에러가 발생하였습니다.\n잠시 후 다시 시도해주세요!");
+    BAD_GATEWAY_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "일시적인 에러가 발생하였습니다.\n잠시 후 다시 시도해주세요!"),
+    Initial_Result_Fail(HttpStatus.INTERNAL_SERVER_ERROR, "결과 초기값 설정에 에러가 발생했습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
