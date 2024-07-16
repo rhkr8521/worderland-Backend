455e470c71dd38947748e5e9dd865360c2ed111c
rhkr8521 <rhkr8521@gmail.com>
2024-07-16 19:04:52 +0900

    [FEAT] Add Images MVC Feature

---
+++ src/main/java/com/rhkr8521/iccas_question/common/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.rhkr8521.iccas_question.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/api/images/**")
+                .addResourceLocations("file:/app/images/");
+    }
+}
